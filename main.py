3d26c765dd275cc262216d126c234aa0b34951bf
wwweeeqqqrrr <joskiychel851@gmail.com>
2026-06-22 20:54:58 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,129 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+from models import Word
+from sqlalchemy import update
+from typing import List
+from sqlalchemy import select, func
+from fastapi import HTTPException,Header,status
+from fastapi.middleware.cors import CORSMiddleware
+from database import get_db
+import os
+import secrets
+import schemas
+
+app = FastAPI(title="EGE-4 API")
+
+async def verify_admin(x_admin_key: str = Header(None)):
+    secret = os.getenv("ADMIN_SECRET")
+    
+
+    if not secret:
+         raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Ошибка конфигурации сервера"
+        )
+  
+    if not x_admin_key or not secrets.compare_digest(x_admin_key, secret):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="У вас нет прав для выполнения этого действия"
+        )
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+     "http://127.0.0.1:5500",
+    "http://localhost:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], #
+)
+@app.post("/words/bulk", response_model=List[schemas.WordResponse])
+async def add_multiple_words(words_list: List[schemas.WordCreate], db: AsyncSession = Depends(get_db),_ = Depends(verify_admin)):
+    """
+    Эндпоинт для добавления сразу нескольких слов (массива).
+    """
+    new_words = [Word(word=item.word, accent_index=item.accent_index) for item in words_list]
+    db.add_all(new_words)
+  
+    
+    try:
+
+        await db.commit()
+        for word in new_words:
+            await db.refresh(word)
+            
+        return new_words
+        
+    except Exception as e:
+        await db.rollback()
+        print(f"Ошибка массового добавления: {e}")
+        raise HTTPException(
+            status_code=400, 
+            detail="Ошибка при сохранении. Возможно, одно или несколько слов уже существуют в базе"
+        )
+    
+
+
+@app.post("/words/", response_model=schemas.WordResponse)
+async def add_word(word: schemas.WordCreate, db: AsyncSession = Depends(get_db),_ = Depends(verify_admin)):
+
+    new_word = Word(word=word.word,accent_index=word.accent_index)
+    db.add(new_word)
+    await db.commit()
+    await db.refresh(new_word)
+    print(new_word)
+    return new_word
+
+
+
+@app.get("/words/random", response_model=List[schemas.WordResponse])
+async def get_random_words(limit: int = 20, db: AsyncSession = Depends(get_db)):
+    """
+    Эндпоинт выдает случайную партию слов для тренировки.
+    По умолчанию 20 штук, но можно запросить любое количество (limit)
+    """
+    
+
+    query = select(Word).order_by(func.random()).limit(limit)
+    
+    result = await db.execute(query)
+    
+    words = result.scalars().all()
+    print(words)
+    return words
+
+@app.delete("/words/{word_id}")
+async def delete_word_by_id(word_id: int, db: AsyncSession = Depends(get_db), _ = Depends(verify_admin)):
+    """
+    Эндпоинт для удаления конкретного слова по его ID из базы данных.
+    Доступно только администратору.
+    """
+  
+    word_to_delete = await db.get(Word, word_id)
+    
+    
+    if not word_to_delete:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Слово с ID {word_id} не найдено"
+        )
+    
+ 
+    await db.delete(word_to_delete)
+    await db.commit()
+    
+    return {"message": f"Слово '{word_to_delete.word}' (ID: {word_id}) успешно удалено"}
+
+@app.post("/danger/reset-db")
+async def danger_reset_db(db: AsyncSession = Depends(get_db), _ = Depends(verify_admin)):
+    await db.execute(text("TRUNCATE TABLE words RESTART IDENTITY"))
+    await db.commit()
+    return {"message": "Таблица слов очищена"}
+
---
+++ script.js
@@ -0,0 +1,86 @@
+const wordsContainer = document.getElementById('words-container');
+
+
+const API_URL = 'http://localhost:8000';
+
+
+const VOWELS = "аеёиоуыэюя";
+let words = []; 
+let correctIndex = -1;
+let word_limit = 30
+
+async function fetchWords() {  // ПОЛУЧЕНИЕ СЛОВ С БЭКЕНДА
+    try {
+        const response = await fetch(`${API_URL}/words/random?limit=${word_limit}`);
+        const data = await response.json();
+     
+        words = data
+        displayCurrentWord()
+        return words
+        
+    } catch (error) {
+        console.error("Ошибка при загрузке слов:", error);
+        wordsContainer.textContent = `Не удалось загрузить слова${error}`;
+    }
+}
+
+function displayCurrentWord() {
+    if (words.length === 0){
+         wordsContainer.innerHTML = `
+            <div style="text-align: center;">
+                <h2>Уверен что точно все запомнил?</h2>
+                <button onclick="location.reload()" style="padding: 10px 20px;font-family: 'Courier New', Courier, monospace; font-size: 18px; cursor: pointer; border-radius: 8px; border: none; background-color: #4CAF50; color: white; margin-top: 20px;">Хочу еще</button>
+            </div>
+        `;
+        return; 
+    }; 
+    wordsContainer.innerHTML = '';
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const wordObject = words.splice(randomIndex, 1)[0];
+    correctIndex = wordObject.accent_index;
+    const poppedWord = wordObject.word.toLowerCase();
+    const wordDiv = document.createElement('div');
+    wordDiv.classList.add('word')
+    for (let i =0; i < poppedWord.length;i++){
+        let type = ''
+        const letter = document.createElement('span')
+        letter.textContent = poppedWord[i]
+        const isVowel = VOWELS.includes(poppedWord[i].toLowerCase());
+        letter.classList.add(isVowel ? 'vowel' : 'consonant');
+        letter.dataset.index = i;
+        wordDiv.appendChild(letter)
+        
+    }
+    
+    wordsContainer.appendChild(wordDiv)
+
+
+
+   
+    
+    
+}
+
+fetchWords();
+
+
+wordsContainer.addEventListener('click',(e)=>{
+    const clickedLetter = e.target.closest('span');
+    if (!clickedLetter) return;
+    
+  
+    if (clickedLetter.classList.contains("vowel") && correctIndex !== -1) {
+        const index = parseInt(clickedLetter.dataset.index);
+        if (index === correctIndex){
+            clickedLetter.classList.add('correct_letter')//добавить зелены цвет
+            setTimeout(displayCurrentWord, 600); 
+
+        }else{
+            clickedLetter.classList.add('incorrect_letter') // добавить красны цвет
+        }
+       
+
+    }
+
+})
+    
